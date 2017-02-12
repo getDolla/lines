407f48802a3fe16bc6829775223cf836620b7c42
getDolla <ywang8@stuy.edu>
2017-02-12 13:50:35 -0500

    tested and done?

--- draw.py
+++ draw.py
@@ -6,16 +6,16 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 
     A = y1 - y0
     B = -(x1 - x0)
-    d = 2A + B
+    d = 2*A + B
 
     while x <= x1:
         plot( screen, color, x, y )
 
         if ( d > 0 ):
             y += 1
-            d += 2B
+            d += 2*B
 
         x += 1
-        d += 2A
+        d += 2*A
 
 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
+draw_line( 0, 0, 460, 100, screen, color )
 
 display(screen)
 save_extension(screen, 'img.png')
