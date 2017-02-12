8a6037cf37db0bcb9b2fc2ff0cf9914f205a9dc9
getDolla <ywang8@stuy.edu>
2017-02-12 13:28:40 -0500

    update

--- draw.py
+++ draw.py
@@ -1,4 +1,21 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    y = y0
+    x = x0
+
+    A = y1 - y0
+    B = -(x1 - x0)
+    d = 2A + B
+
+    while x <= x1:
+        plot( screen, color, x, y )
+
+        if ( d > 0 ):
+            y += 1
+            d += 2B
+
+        x += 1
+        d += 2A
+
+
