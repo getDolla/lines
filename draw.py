3f939fda93a360b374af189dcf992b20ef93dd14
getDolla <ywang8@stuy.edu>
2017-02-14 00:29:48 -0500

    added other octant capabilities

--- draw.py
+++ draw.py
@@ -1,11 +1,6 @@
 from display import *
 
-def draw_line( x0, y0, x1, y1, screen, color ):
-    y = y0
-    x = x0
-
-    A = y1 - y0
-    B = -(x1 - x0)
+def draw_o1( x, y, A, B, x1, y1, screen, color ):
     d = 2*A + B
 
     while x <= x1:
@@ -17,5 +12,62 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 
         x += 1
         d += 2*A
+        
+def draw_o2( x, y, A, B, x1, y1, screen, color ):
+    d = 2*B + A
 
+    while y <= y1:
+        plot( screen, color, x, y )
+
+        if ( d < 0 ):
+            x += 1
+            d += 2*A
+
+        y += 1
+        d += 2*B
+
+def draw_o8( x, y, A, B, x1, y1, screen, color ):
+    d = 2*A - B
+
+    while x <= x1:
+        plot( screen, color, x, y )
+
+        if ( d < 0 ):
+            y -= 1
+            d -= 2*B
+
+        x += 1
+        d += 2*A
+    
+def draw_o7( x, y, A, B, x1, y1, screen, color ):
+    d = A - 2*B
+
+    while y >= y1:
+        plot( screen, color, x, y )
+
+        if ( d > 0 ):
+            x += 1
+            d += 2*A
+
+        y -= 1
+        d -= 2*B
+
+def draw_line( x0, y0, x1, y1, screen, color ):
+    y = y0
+    x = x0
+
+    A = y1 - y0
+    B = -(x1 - x0)
+
+    if ( (A <= -B) and (A >= 0) ):
+        draw_o1( x, y, A, B, x1, y1, screen, color )
+
+    elif ( (A > -B) and (A > 0) ):
+        draw_o2( x, y, A, B, x1, y1, screen, color )
+
+    elif ( (A >= B) and (A < 0) ):
+        draw_o8( x, y, A, B, x1, y1, screen, color )
+
+    else:
+        draw_o7( x, y, A, B, x1, y1, screen, color )
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-draw_line( 0, 0, 460, 100, screen, color )
+draw_line( 0, 499, 200, 0, screen, color )
 
 display(screen)
 save_extension(screen, 'img.png')
