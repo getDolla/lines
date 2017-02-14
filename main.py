6f0b06888c5d35c1eb666a6015205080107cfa5b
getDolla <ywang8@stuy.edu>
2017-02-14 11:06:15 -0500

    drawing a stickman

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
-draw_line( 0, 499, 200, 0, screen, color )
+
+color = [ 255, 255, 255 ]
+
+#top of the head
+draw_line( XRES/2 - 30, YRES-60, XRES/2 + 30, YRES-60, screen, color )
+
+draw_line( XRES/2 - 50, YRES-80, XRES/2 - 30, YRES-60, screen, color )
+
+draw_line( XRES/2 + 30, YRES-60, XRES/2 + 50, YRES-80, screen, color )
+
+#sides of the head
+draw_line( XRES/2 - 50, YRES-80, XRES/2 - 50, YRES-140, screen, color )
+
+draw_line( XRES/2 + 50, YRES-80, XRES/2 + 50, YRES-140, screen, color )
+
+#bottom of the head
+draw_line( XRES/2 - 50, YRES-140, XRES/2 - 30, YRES-160, screen, color )
+
+draw_line( XRES/2 - 30, YRES-160, XRES/2 + 30, YRES-160, screen, color )
+
+draw_line( XRES/2 + 30, YRES-160, XRES/2 + 50, YRES-140, screen, color )
+
+#main body
+
 
 display(screen)
 save_extension(screen, 'img.png')
